3f27378217b91c07c4f70aac28e7c38381d6d0ed
MRzal <38412845+MRzal@users.noreply.github.com>
2019-11-30 20:22:55 +0700

    komat kamit

---
+++ resources/lang/en/custom-menu.php
@@ -0,0 +1,17 @@
+<?php return array (
+  'nav-menu' => 
+  array (
+    'blog' => 'Blog',
+    'courses' => 'Courses',
+    'bundles' => 'Bundles',
+    'forums' => 'Forums',
+    'contact' => 'Contact',
+    'about-us' => 'About Us',
+  ),
+);return array (
+  'nav-menu' => 
+  array (
+    'courses' => 'Courses',
+    'about-us' => 'About Us',
+  ),
+);
